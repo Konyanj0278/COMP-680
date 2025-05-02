5a6c2f5b792dfef5b377b4f28e343e50119e6cb5
shyamthummar8787 <168592099+shyamthummar8787@users.noreply.github.com>
2025-05-02 14:58:56 -0700

    fixed logout error

--- login/login.py
+++ login/login.py
@@ -238,7 +238,7 @@ class __login__:
                 st.session_state['LOGGED_IN'] = False
                 self.cookies['__streamlit_login_signup_ui_username__'] = '1c9a923f-fb21-4a91-b3f3-5f18e3f01182'
                 del_logout.empty()
-                st.experimental_rerun()
+                st.rerun()
         
 
     def nav_sidebar(self):
